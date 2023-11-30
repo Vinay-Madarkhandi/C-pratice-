6d841a5ddc3ed458b7960f8d6ea1db31961bb8b3
Madarkhandi Vinay Shantappa <139522065+Vinay-Madarkhandi@users.noreply.github.com>
2023-11-30 15:39:18 +0530

    first commit

---
+++ hello.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+ 
+    int  a, b, c;
+    cout << "Enter a number: ";
+    cin >> a;
+    cout << "Enter another number: ";
+    cin >> b;
+    
+    c = a + b;
+    cout << "Your answer is " << c << endl;
+
+    return 0;
+}
